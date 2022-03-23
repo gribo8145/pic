c0f3317ded7f0f771dbf7142cba9e3b91f688a3a
vladimirzva <38539866+vladimirzva@users.noreply.github.com>
2022-03-23 17:26:11 +0300

    Правки

---
+++ src/main/java/Pic.java
@@ -0,0 +1,11 @@
+public class Pic {
+    public static void main(String[] args){
+
+        System.out.println("Hello, worldяывафывп");
+        double l = 8.8;
+        double s = l*l;
+        System.out.println("Плащадь квадрата со стороной l" + l + "=" + s);
+
+
+    }
+}
