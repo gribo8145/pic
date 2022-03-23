c8a2929c5d15474d2cb3ee7cb4de335d0f95c6e2
vladimirzva <38539866+vladimirzva@users.noreply.github.com>
2022-03-23 18:45:48 +0300

    второе задание

--- src/main/java/Pic.java
+++ src/main/java/Pic.java
@@ -1,11 +1,32 @@
 public class Pic {
     public static void main(String[] args){
 
-        System.out.println("Hello, world");
-        double l = 8.8;
-        double s = l*l;
-        System.out.println("Плащадь квадрата со стороной l" + l + "=" + s);
+        hello("world");
+        hello("user");
+        hello("vova");
+        double a = 4;
+        double b = 5;
+        System.out.println(area(a,b));
 
+        int  f = 5;
+        int d = 6;
+        System.out.println(klm(f, d));
 
+           }
+
+           
+           public static void hello(String somebody){
+
+               System.out.println("Hello " + somebody + "!");
+           }
+
+           public static double area(double a, double b){
+        return a * b;
+
+            }
+    public static int klm(int f, int d)
+    {
+
+        return f * d;
     }
 }
